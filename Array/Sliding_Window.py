89df53ed784ebd3402fd0b637d4a163574f25834
devbyashu <77873994+thehunktown@users.noreply.github.com>
2025-04-02 22:51:38 +0530

    initializing array - sliding window

---
+++ Array/Sliding_Window.py
@@ -0,0 +1,19 @@
+# Sliding window: Efficiently solves subarray problems with fixed or variable window sizes
+# Time complexity: O(n), where n is the size of the input array'
+# Space complexity: O(n), where n is the size of the input array'
+'''
+[1, 3, 5, 2, 8, 1, 5]
+[1, 3, 5] 2  8  1  5   → Sum = 9  
+1 [3, 5, 2] 8  1  5   → Sum = 10  
+1  3 [5, 2, 8] 1  5   → Sum = 15  
+1  3  5 [2, 8, 1] 5   → Sum = 11  
+1  3  5  2 [8, 1, 5]  → Sum = 14  
+
+'''
+
+class Array:
+    def __init__():
+        pass
+    
+    def sliding_window():
+        # Initialize the input array and the window size
